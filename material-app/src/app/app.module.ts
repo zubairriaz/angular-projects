bd23d30abb31cff978104bbedc050ab104f39951
Zubair <zubair.riaz@trapezegroup.com>
2021-07-25 21:04:08 +0500

    commtting changes

--- material-app/src/app/app.module.ts
+++ material-app/src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button"
+import {MatIconModule} from "@angular/material/icon"
+import {MatButtonToggleModule} from "@angular/material/button-toggle"
+import {MatFormField} from "@angular/material/form-field"
+import {MatInput} from "@angular/material/input"
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatFormField,
+    MatInput
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
